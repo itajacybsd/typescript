cd7487de582b8ba71d5e1af75015f4a7b1a2ff1d
itajacy TI02 <itajacy@bsd.com.br>
2025-12-09 08:51:35 -0300

    my first commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const x: number =3;
+
+console.log(x * 7 + 10);
+
---
+++ test/calc.test.ts
@@ -0,0 +1,9 @@
+import { somar } from "../src/calc";
+
+test('Deve somar corretamente',() => {
+
+  const resultado = somar(12, 20);
+  expect(resultado).toBe(32);
+
+}) 
+
