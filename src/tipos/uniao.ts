ccec39283eac93225294a62328a8eab16fff3139
ita Dev <itajacy@gmail.com>
2025-12-09 19:33:41 -0300

    aula 17 - checagem de tipos

--- src/tipos/uniao.ts
+++ src/tipos/uniao.ts
@@ -2,3 +2,20 @@ let nota: number | string = 10;
 nota = "A+";
 
 console.log(`Minha nota é ${nota}`);
+
+type NotaOuConceito = number | 'A' | 'B' | 'C' | 'D' | 'E' | 'F'  ;
+
+let n1: NotaOuConceito = 7;
+n1 = 'C';
+
+function imprimirNota(nota: NotaOuConceito) {
+    if(typeof nota === 'number') {
+        console.log(`Nota ${nota}`)
+    } else {
+        console.log(`Conceito ${nota}`)
+
+    }
+}
+
+// n1 = 9.5;
+imprimirNota(n1);
