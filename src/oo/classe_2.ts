a7231b9b7e074caf55899a7cc5c9a42fa57945cf
itajacy TI02 <itajacy@bsd.com.br>
2025-12-10 13:24:58 -0300

    Aula 29 - atributos via construtor

--- src/oo/classe_2.ts
+++ src/oo/classe_2.ts
@@ -1,9 +1,12 @@
 class Curso {
-    readonly id: number;
-    public nome: string | undefined;
+    //  id: number;
+    // public nome: string | undefined;
 
-    constructor(id: number) {
-        this.id = id;
+    // colocando o modificador readonly no construtor não é necessário declarar o atributo antes
+    // assim ele passa a ser entendido como um atributo da instancia
+
+    // e ainda colocando o modificador public no atributo nome, ele também passa a ser um atributo da instancia
+    constructor(readonly id: number, public nome?: string) {
         if (id <1) throw new Error("ID inválido");
     }
 
@@ -14,3 +17,7 @@ const c1 = new Curso(123);
 c1.nome = "Iniciando com TypeScript";
 console.log(c1.id);
 console.log(c1.nome);
+
+const c2 = new Curso(456, "TypeScript Avançado");
+console.log(c2.id);
+console.log(c2.nome);
