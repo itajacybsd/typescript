983026cdff91814ef15d18f9ccff64976a6dbeac
itajacy TI02 <itajacy@bsd.com.br>
2025-12-10 11:35:56 -0300

    Aula 26 - Atributos privados, get e set

--- src/oo/classe_1.ts
+++ src/oo/classe_1.ts
@@ -1,9 +1,22 @@
 class Curso {
-    nome: string | undefined;
+    private _id: number | undefined;
+    public nome: string | undefined;
+
+    get id(): number | undefined {
+        return this._id;
+    }
+
+    set id(id: number | undefined) {
+        if ((id ?? 0) > 0) {
+            this._id = id;
+        }
+    }
 
 
 }
 
 const c1 = new Curso();
+c1.id = 123;
 c1.nome = "Iniciando com TypeScript";
+console.log(c1.id);
 console.log(c1.nome);
