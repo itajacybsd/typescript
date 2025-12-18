b166cc8831ffaf6d01df8efa95aefc9a82aeb6c5
itajacy TI02 <itajacy@bsd.com.br>
2025-12-18 11:33:53 -0300

    aula 52 - Heranca #3 - exemplo de caso de uso

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@
 // import './interfaces/funcao';
 // import './pilares_oo/encapsulamento';
 // import './pilares_oo/heranca_1';
-import './pilares_oo/heranca_2';
+// import './pilares_oo/heranca_2';
+import './pilares_oo/heranca_3';
 
 
--- src/pilares_oo/heranca_3.ts
+++ src/pilares_oo/heranca_3.ts
@@ -1,4 +1,42 @@
+// exemplo de caso de uso:  - registrar um usuario na aplicacao
+// logar um usuario na aplicacao
+// pagar um boleto
+// buscar produtos no catalogo
+// adicionar produto no carrinho de compras
+// finalizar compra
+
+
+interface Usuario {
+    id: number;
+    nome: string;
+    email: string;
+    senha: string;
+}
+interface CasoDeUso {
+    executar(entrada: any): any;
+}
+
+class RegistrarUsuario implements CasoDeUso {
+    executar(usuario: Usuario) {
+        console.log(`Executando o caso de uso de registrar usuario`);
+        console.log(usuario);
+        // logica de registrar o usuario no sistema
+        // return true;
+    }
+}
 
 
 
-export {}
+const u1: Usuario = {
+    id: 321,
+    nome: 'João',
+    email: 'joao@zmail.com',
+    senha: '123456'
+}
+
+const casoDeUso: CasoDeUso = new RegistrarUsuario();
+casoDeUso.executar(u1);
+
+
+
+export { }
